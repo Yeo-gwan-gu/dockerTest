277eed83f1a4b4dcd44516a8663e3748b5c8f329
ygg <rnrn4728@gmail.com>
2024-04-25 15:00:44 +0900

    세팅

--- src/main/java/com/example/dockertest/IndexController.java
+++ src/main/java/com/example/dockertest/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.dockertest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    @Value("${index.string}")
+    private String indexString;
+
     @GetMapping("/index")
     public ResponseEntity<String> index() {
-        return ResponseEntity.ok().body("hello");
+        return ResponseEntity.ok().body(indexString); // index.string 값 확인
     }
 
     @GetMapping("/welcome")
